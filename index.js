c883bde15fd2a05b3b7d5e8735a1be7eb772b466
Esther <estherchidozie36@gmail.com>
2025-03-27 02:45:54 -0700

    fix

--- index.js
+++ index.js
@@ -14,10 +14,15 @@ const PORT = process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
 
+// const corOptions ={
+//     origin: process.env.CLIENT_URL || ['http://localhost:3000'],
+//     credential: true,
+//     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
+// }
+
 const corOptions ={
     origin: process.env.CLIENT_URL || ['http://localhost:3000'],
-    credential: true,
-    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
+        methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'] 
 }
 
 app.use(cors(corOptions))
